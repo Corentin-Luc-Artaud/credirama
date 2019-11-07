7379e85218d1f3a684ae5a152351240c9822860e
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2019-11-07 15:20:12 +0100

    Trying to use TimeService when creating Bank Accounts

--- clientService/src/main/java/fr/unice/polytech/si5/al/clientService/components/ClientRegisterer.java
+++ clientService/src/main/java/fr/unice/polytech/si5/al/clientService/components/ClientRegisterer.java
@@ -4,27 +4,29 @@ import com.google.gson.Gson;
 import fr.unice.polytech.si5.al.clientService.kafka.Producer;
 import fr.unice.polytech.si5.al.clientService.models.Client;
 import fr.unice.polytech.si5.al.clientService.repositories.ClientRepository;
+import fr.unice.polytech.si5.al.clientService.services.TimeService;
 import fr.unice.polytech.si5.al.models.events.CrediramaEvent;
 import fr.unice.polytech.si5.al.models.events.EventName;
 import fr.unice.polytech.si5.al.models.events.EventPhase;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-
-
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 @Component
 public class ClientRegisterer {
 
-    @Autowired
-    private ClientRepository clientRepository;
+    private final SimpleDateFormat simpleDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+
+    private final ClientRepository clientRepository;
+    private final TimeService timeService;
 
     private static final Logger logger = LogManager.getLogger("ClientRegisterer");
 
@@ -32,16 +34,25 @@ public class ClientRegisterer {
 
     private Gson gson;
 
+    public ClientRegisterer(ClientRepository clientRepository, TimeService timeService) {
+        this.clientRepository = clientRepository;
+        this.timeService = timeService;
+    }
+
     public long addNewClient(Client newClient) {
         logger.info("New client has been registered");
         clientRepository.save(newClient);
         logger.info("New bank account has been created for client " + newClient.getFirstName() + " " + newClient.getLastName());
 
+        LocalDateTime currentTime = timeService.getCurrentTime();
+        long currentLongTime = currentTime.toLocalDate().toEpochDay();
+        String isoTime = simpleDate.format(new Timestamp(currentLongTime));
+
         CrediramaEvent eventMessage = CrediramaEvent.builder()
                 .eventName(EventName.CLIENT_SUBSCRITION)
                 .eventPhase(EventPhase.PRODUCTION)
                 .payload(newClient)
-                .timestamp(setRandomTime())
+                .timestamp(isoTime)
                 .build();
         try {
             String message = gson.toJson(eventMessage);
@@ -60,12 +71,13 @@ public class ClientRegisterer {
         return res;
     }
 
+    @Deprecated
     private String setRandomTime() {
 
         long offset = Timestamp.valueOf("2019-01-01 00:00:00").getTime();
         long end = Timestamp.valueOf("2019-11-01 00:00:00").getTime();
         long diff = end - offset + 1;
-        Timestamp rand = new Timestamp(offset + (long)(Math.random() * diff));
+        Timestamp rand = new Timestamp(offset + (long) (Math.random() * diff));
 
         // Conversion
         SimpleDateFormat sdf;
