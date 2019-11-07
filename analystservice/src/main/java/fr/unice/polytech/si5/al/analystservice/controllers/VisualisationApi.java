0d6995ced6fd01e4c613187e31d615c5b1ccbaa3
Walid Larabi <walid.larabi@etu.unice.fr>
2019-11-07 23:47:26 +0100

    Fixing creating timelion request

--- analystservice/src/main/java/fr/unice/polytech/si5/al/analystservice/controllers/VisualisationApi.java
+++ analystservice/src/main/java/fr/unice/polytech/si5/al/analystservice/controllers/VisualisationApi.java
@@ -1,13 +1,8 @@
 package fr.unice.polytech.si5.al.analystservice.controllers;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import fr.unice.polytech.si5.al.analystservice.Main;
-import fr.unice.polytech.si5.al.analystservice.services.CreateTimelionRequest;
+import fr.unice.polytech.si5.al.analystservice.services.CreateTimelionService;
 import org.json.*;
-
-import fr.unice.polytech.si5.al.analystservice.services.Kibana;
-import fr.unice.polytech.si5.al.analystservice.services.VisualisationServices;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -18,10 +13,14 @@ public class VisualisationApi {
 
     public static Route addVisualisationRoute = (request, response) -> {
         logger.info("VisualisationApi::addVisualisationRoute called");
-        Kibana kibana = new Kibana();
-        VisualisationServices vizServ = new VisualisationServices();
-        JSONObject jsonObject = new JSONObject(request.body());
-        kibana.addVisualisation(vizServ.createTimelion(jsonObject.get("title").toString(), jsonObject.get("expression").toString()));
+
+        // get body
+        JSONObject body = new JSONObject(request.body());
+
+        // call create timelion service
+        CreateTimelionService createTimelionService = new CreateTimelionService();
+        createTimelionService.createTimelion(body.get("title").toString(), body.get("expression").toString());
+
         return "ok";
     };
 
