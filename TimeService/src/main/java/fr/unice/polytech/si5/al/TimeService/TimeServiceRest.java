65d1a59bd344c847c54918c71ede0c17ec15e7a5
corentin-luc-artaud <corentin.luc.artaud@gmail.com>
2020-01-31 09:12:18 +0100

    new interface for time service

--- TimeService/src/main/java/fr/unice/polytech/si5/al/TimeService/TimeServiceRest.java
+++ TimeService/src/main/java/fr/unice/polytech/si5/al/TimeService/TimeServiceRest.java
@@ -3,9 +3,8 @@ package fr.unice.polytech.si5.al.TimeService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Date;
 import java.time.LocalDateTime;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.logging.Logger;
 
 @RestController
@@ -15,39 +14,43 @@ public class TimeServiceRest {
     @Value("${failpercentage:5}") // default 1/20
     private double percentage_of_fail;
 
-    private LocalDateTime curTime;
+    private long curTimeMillis;
     private double failValue;
     //private Random random;
 
     public TimeServiceRest() {
-        this.curTime = LocalDateTime.now();
-        //this.random = new Random(System.nanoTime());
+        this.curTimeMillis = System.currentTimeMillis();
         failValue = Math.random()*100;
     }
 
     @PostMapping(value = "/", consumes = "text/plain")
-    public String setTime(@RequestBody String string) {
+    public String setTime(@RequestBody long timestamp) {
         //System.out.println("Accessed POST - " + string);
         failValue = Math.random()*100;
-        curTime = LocalDateTime.parse(string);
+        curTimeMillis = timestamp;
         return "OK";
     }
 
     @PostMapping("/fail")
     public void failNow() {
-        curTime = curTime.minusMinutes(4);
+        curTimeMillis = curTimeMillis-(4*60*1000);
         failValue = -1;
     }
 
+    @PostMapping("/recover")
+    public void Recover() {
+
+    }
+
     @GetMapping(value = "/")
-    public String getTimeWithFail() {
+    public long getTimeWithFail() {
         tryFail();
         return getTime();
     }
 
-    public String getTime() {
+    public long getTime() {
         //System.out.println("Accessed GET - " + curTime);
-        return curTime.toString();
+        return curTimeMillis;
     }
 
 
@@ -55,10 +58,10 @@ public class TimeServiceRest {
      * actualy fail one on 10 times
      */
     public void tryFail() {
+        if (percentage_of_fail <= 0) return;
         if (failValue <= percentage_of_fail && failValue >= 0) {
             Logger.getLogger(this.getClass().getName()).info("Fail !!!");
-            curTime = curTime.minusMinutes(4);
-            failValue = -1;
+            failNow();
         }else if(failValue > 0){
             failValue -= percentage_of_fail;
         }
